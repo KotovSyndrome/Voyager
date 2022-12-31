5388f24268779db5dfc949390d894e33b7260669
Philip Ziolkowski <philipz4848@gmail.com>
2022-12-31 01:54:31 -0800

    Adding layout for trip creation page and react-calendar package for choosing dates

--- src/pages/trips/index.tsx
+++ src/pages/trips/index.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import { FaPlane } from 'react-icons/fa'
 import TripCard from '../../components/tripCard'
 
 const trips = () => {
     const [toggle, setToggle] = useState(false)
+    const router = useRouter()
 
   return (
     <div className='relative w-4/6 m-auto'>
@@ -13,7 +15,7 @@ const trips = () => {
             <p className='text-3xl mt-8'>
                 <span onClick={() => setToggle(!toggle)} className={`${!toggle && 'underline underline-offset-8 decoration-cyan-600'} cursor-pointer`}>Upcoming</span>  |  <span onClick={() => setToggle(!toggle)} className={`${toggle && 'underline underline-offset-8 decoration-cyan-600'} cursor-pointer`}>Past</span>
             </p>
-            <button className='bg-sky-300 text-slate-50 px-8 py-2 rounded-md absolute top-7 right-1 hover:bg-sky-500'>Plan trip <span className='inline-block text-md'><FaPlane/></span></button>
+            <button onClick={() => router.push('/trips/plan')} className='bg-sky-300 text-slate-50 px-8 py-2 rounded-md absolute top-7 right-1 hover:bg-sky-500'>Plan trip <span className='inline-block text-md'><FaPlane/></span></button>
         </div>
 
         <TripCard title={'Euro super trip'} startDate={new Date()} endDate={new Date()} collaborators={['Jason', 'Chris', 'Henry']} id={1} />
