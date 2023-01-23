c063b47c530af40929d8a52929ddaa2e155e3465
Philip Ziolkowski <philipz4848@gmail.com>
2023-01-22 22:06:57 -0800

    adding state profile form, changing input attribute values, changing semantic HTML elements

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,8 +5,6 @@ import ProfilePlaceholder from '../assets/profile-placeholder.png'
 import Image from 'next/image'
 import { getServerAuthSession } from '../server/common/get-server-auth-session'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { authOptions } from './api/auth/[...nextauth]'
-import { profileEnd } from 'console'
 
 
 interface IProfile {
@@ -35,10 +33,24 @@ interface IProfileData {
 
 
 const profile = (profileData: IProfileData) => {
-  const [editing, setEditing] = useState(true)
+  const [editing, setEditing] = useState(false)
+  const [formValues, setFormValues] = useState({
+    username: profileData.profile.username || profileData.user.name,
+    avatarPic: '',
+    bio: profileData.profile.bio,
+    distanceUnits: profileData.profile.distanceUnits,
+    dateFormat: profileData.profile.dateFormat,
+    timeFormat: profileData.profile.timeFormat,
+    commentsNotification: profileData.profile.commentsNotification,
+    remindersNotification: profileData.profile.remindersNotification,
+    collaboratorNotification: profileData.profile.collaboratorJoinedNotification
+  })
 
-  const handleProfileEdit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    //
+  const handleProfileEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // setEditing(editing => !editing)
+    console.log('hello')
   } 
 
   return (
@@ -46,71 +58,77 @@ const profile = (profileData: IProfileData) => {
         <div className='m-auto lg:w-3/6 md:w-4/6 mt-16'>
           <div className='bg-blue-100 rounded-lg py-4 px-8'>
 
-            <div className=' py-3 px-6 rounded-lg flex space-x-10 items-center'>
+            <div className=' py-3 px-6 rounded-lg flex items-center'>
               <div>
-                <p className='mb-3 text-center text-xl'>{profileData.profile.username || profileData.user.name}</p>
+                <input type='text' disabled={!editing} defaultValue={profileData.profile.username || profileData.user.name} name='username' className='border-0 outline-none bg-transparent mb-3 text-xl'/>
                 <Image src={profileData.user.image || ProfilePlaceholder} alt='Profile avatart' width={100} height={100} className='rounded-full'/>
               </div>
-              {/* @ts-ignore */}
-              <p className='bg-slate-50 p-2 rounded-lg w-full '>{profileData.profile.bio || 'Tell the world what kind of traveler you are!'}</p>
+              <textarea disabled={!editing} defaultValue={profileData.profile.bio || 'Tell the world what kind of traveler you are!'} name='bio' className='bg-slate-50 p-2 rounded-lg w-full border-0 resize-none'></textarea>
             </div>
 
-            <form onSubmit={handleProfileEdit} className='mt-5 bg-slate-50 px-4 pt-4 rounded-lg'>
+            <section className='mt-5 bg-slate-50 px-4 pt-4 rounded-lg'>
               <div className='flex flex-col space-y-3'>
                   <h3 className='text-center text-2xl font-semibold'>Preferences</h3>
 
-                  <div>
-                    <label htmlFor='email'>Email:</label>
-                    <input disabled={true} name='email'value={profileData.user.email}  className='border-[1px] border-gray-200 py-1 px-3 ml-2 rounded-md'/>
-                  </div>
+                  <form onSubmit={handleProfileEdit} id='profileForm'>
+                    <div>
+                      <label htmlFor='email'>Email:</label>
+                      <input disabled={true} name='email'value={profileData.user.email}  className='border-[1px] border-gray-200 py-1 px-3 ml-2 rounded-md'/>
+                    </div>
 
-                  <div>
-                    <label htmlFor='units'>Distance Units: </label>
-                    <select disabled={true} defaultValue={profileData.profile.distanceUnits} name='units' className='rounded-md p-1 ml-1 border-gray-200'>
-                      <option>Miles</option>
-                      <option>Kilometers</option>
-                      <option>bananas</option>
-                    </select>
-                  </div>
+                    <div className='flex items-center space-x-2'>
+                      <label htmlFor='units'>Distance Units: </label>
+                      <input type='radio' value={'MILES'} disabled={!editing} defaultChecked={profileData.profile.distanceUnits === 'MILES'} id='milesFormat' name='distanceUnits' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='milesFormat'>Miles (mi)</label>
 
-                  <div className='flex items-center space-x-2'>
-                    <label>Date format: </label>
-                    <input value={'MONTH'} type='radio' id='monthFirst' readOnly={editing} defaultChecked={profileData.profile.dateFormat === 'MONTH' && true} name='dateFormatPreference' className='rounded-md text-sky-400 border-gray-300'/>
-                    <label htmlFor='monthFirst'>Month/Day <span className='text-gray-400 text-sm'>(5/23)</span></label>
-                    <input value={'DAY'} type='radio' id='dayFirst' readOnly={editing} defaultChecked={profileData.profile.dateFormat === 'DAY' && true} name='dateFormatPreference' className='rounded-md text-sky-400 border-gray-300'/>
-                    <label htmlFor='dayFirst'>Day/Month <span className='text-gray-400 text-sm'>(23/5)</span></label>
-                  </div>
+                      <input type='radio' value={'KILOMETERS'} disabled={!editing} defaultChecked={profileData.profile.distanceUnits === 'KILOMETERS'} id='kilometersFormat' name='distanceUnits' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='kilometersFormat'>Kilometers (km)</label>
 
-                  <div className='flex items-center space-x-2'>
-                    <label>Time format:</label>
-                    <input type='radio' id='12' readOnly={editing} value={'12'} defaultChecked={profileData.profile.timeFormat === 'TWELVE' && true} name='timeFormatPreference' className='rounded-md text-sky-400 border-gray-300'/>
-                    <label htmlFor='12'>12h <span className='text-gray-400 text-sm'>(1:30pm)</span></label>
-                    <input type='radio' id='24' readOnly={editing} value={'24'} defaultChecked={profileData.profile.timeFormat === 'TWENTYFOUR' && true} name='timeFormatPreference' className='rounded-md text-sky-400 border-gray-300'/>
-                    <label htmlFor='24'>24h <span className='text-gray-400 text-sm'>(13:30)</span></label>
-                  </div>
+                      <input type='radio' value={'BANANAS'} disabled={!editing} defaultChecked={profileData.profile.distanceUnits === 'BANANAS'} id='bananasFormat' name='distanceUnits' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='bananasFormat'>Bananas (🍌)</label>
+                    </div>
 
-                  <h3 className='text-center text-2xl font-semibold'>Email Notifications</h3>
+                    <div className='flex items-center space-x-2'>
+                      <label>Date format: </label>
+                      <input value={'MONTH'} type='radio' id='monthFirst' disabled={!editing} defaultChecked={profileData.profile.dateFormat === 'MONTH'} name='dateFormat' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='monthFirst'>Month/Day <span className='text-gray-400 text-sm'>(5/23)</span></label>
+                      <input value={'DAY'} type='radio' id='dayFirst' disabled={!editing} defaultChecked={profileData.profile.dateFormat === 'DAY'} name='dateFormat' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='dayFirst'>Day/Month <span className='text-gray-400 text-sm'>(23/5)</span></label>
+                    </div>
 
-                  <div className='flex items-center space-x-2'>
-                    <input type='checkbox'  value={'true'} defaultChecked={profileData.profile.commentsNotification && true} className='border-gray-300 rounded-md text-sky-400'></input>
-                    <label >Someone commented on your itinerary</label>
-                  </div>
+                    <div className='flex items-center space-x-2'>
+                      <label>Time format:</label>
+                      <input type='radio' id='12' disabled={!editing} value={'12'} defaultChecked={profileData.profile.timeFormat === 'TWELVE'} name='timeFormat' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='12'>12h <span className='text-gray-400 text-sm'>(1:30pm)</span></label>
+                      <input type='radio' id='24' disabled={!editing} value={'24'} defaultChecked={profileData.profile.timeFormat === 'TWENTYFOUR'} name='timeFormat' className='rounded-md text-sky-400 border-gray-300'/>
+                      <label htmlFor='24'>24h <span className='text-gray-400 text-sm'>(13:30)</span></label>
+                    </div>
 
-                  <div className='flex items-center space-x-2'>
-                    <input type='checkbox'  value={'true'} defaultChecked={profileData.profile.remindersNotification && true} className='border-gray-300 rounded-md text-sky-400'></input>
-                    <label>Reminders about your upcoming trip</label>
-                  </div>
+                    <h3 className='text-center text-2xl font-semibold'>Email Notifications</h3>
 
-                  <div className='flex items-center space-x-2'>
-                    <input type='checkbox'  value={'true'} defaultChecked={profileData.profile.collaboratorJoinedNotification && true} className='border-gray-300 rounded-md text-sky-400'></input>
-                    <label>Your friend joined your trip as a collaborator</label>
-                  </div>
+                    <div className='flex items-center space-x-2'>
+                      <input type='checkbox' disabled={!editing} value={'true'} defaultChecked={profileData.profile.commentsNotification} id='commented' name='commentsNotification' className='border-gray-300 rounded-md text-sky-400'></input>
+                      <label htmlFor='commented'>Someone commented on your itinerary</label>
+                    </div>
+
+                    <div className='flex items-center space-x-2'>
+                      <input type='checkbox' disabled={!editing} value={'true'} defaultChecked={profileData.profile.remindersNotification} id='reminders' name='remindersNotification' className='border-gray-300 rounded-md text-sky-400'></input>
+                      <label htmlFor='reminders'>Reminders about your upcoming trip</label>
+                    </div>
+
+                    <div className='flex items-center space-x-2'>
+                      <input type='checkbox' disabled={!editing} value={'true'} defaultChecked={profileData.profile.collaboratorJoinedNotification} id='friendJoined' name='collaboratorNotification' className='border-gray-300 rounded-md text-sky-400'></input>
+                      <label htmlFor='friendJoined'>Your friend joined your trip as a collaborator</label>
+                    </div>
+                  </form>
 
                   <div className='flex justify-center'>
-                    <button type='submit' className='bg-orange-300 py-2 px-6 mb-2 rounded-lg text-slate-50 hover:bg-orange-400'>Edit profile</button>
+                    {/* Rendering buttons like this so they are not both submitting the form. Rendering with ternary causes both to submit the form for some reason🤔 */}
+                    {editing && <button type='submit' form='profileForm' className='bg-green-300 py-2 px-6 mb-2 rounded-lg text-slate-50 hover:bg-green-400'>Save Changes</button>}
+                    {!editing && <button type='button' onClick={() => setEditing(!editing)} className='bg-orange-300 py-2 px-6 mb-2 rounded-lg text-slate-50 hover:bg-orange-400'>Edit profile</button>}
                   </div>
                 </div>
-            </form>
+            </section>
           </div>
 
           <div className='flex justify-center mt-10'>
