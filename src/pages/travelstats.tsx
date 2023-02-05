809e2f091c4e71d95b5ce01a8c6f4d5b990d1eea
Philip Ziolkowski <philipz4848@gmail.com>
2023-02-04 21:31:14 -0800

    Adjusting grid for mobile, Changing graph name

--- src/pages/travelstats.tsx
+++ src/pages/travelstats.tsx
@@ -3,7 +3,8 @@ import GraphCard from '../components/graphCard'
 import LayoutWrapper from '../components/layoutWrapper'
 
 const graphs = [
-  'Miles traveled vs National Average',
+  // Compare to national average?
+  'Distance traveled',
   'Cities visited per trip',
   'Distribution of method of travel',
   'Percentage of the world seen'
@@ -14,7 +15,7 @@ const travelstats = () => {
 
     <LayoutWrapper>
       <div className='w-full'>
-        <div className='grid grid-cols-2 gap-10 mt-10 pb-10'>
+        <div className='grid grid-cols-1 xl:grid-cols-2 gap-10 mt-10 pb-10'>
           {graphs.map(g => {
             return <GraphCard key={g} title={g}/>
           })}
