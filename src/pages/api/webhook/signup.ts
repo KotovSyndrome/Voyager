047b71ea150b640ec0b8dbf12e06bfcfc28a3f38
Philip Ziolkowski <philipz4848@gmail.com>
2023-04-09 13:31:46 -0700

    changing types

--- src/pages/api/webhook/signup.ts
+++ src/pages/api/webhook/signup.ts
@@ -29,7 +29,7 @@ type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
 };
   
 type Event = {
-    data: User;
+    data: UserData;
     object: "event";
     type: EventType;
 };
@@ -67,11 +67,12 @@ export default async function handler(
     if (evt.data) {
         console.log('Hit attributes block')
 
-      const emailObject = evt.data?.emailAddresses?.find((email) => {
-        return email.id === evt?.data.primaryEmailAddressId;
+      const emailObject = evt.data?.email_addresses?.find((email) => {
+        return email.id === evt?.data.primary_email_address_id;
       });
 
       if (!emailObject) {
+        console.log('no email obj')
         return res.status(400).json({});
       }
 
@@ -94,3 +95,62 @@ export default async function handler(
   }
 }
 
+export interface UserData {
+    backup_code_enabled:      boolean;
+    banned:                   boolean;
+    birthday:                 string;
+    created_at:               number;
+    email_addresses:          EmailAddress[];
+    external_accounts:        ExternalAccount[];
+    external_id:              null;
+    first_name:               string;
+    gender:                   string;
+    id:                       string;
+    image_url:                string;
+    last_name:                null;
+    last_sign_in_at:          null;
+    object:                   string;
+    password_enabled:         boolean;
+    phone_numbers:            any[];
+    primary_email_address_id: string;
+    primary_phone_number_id:  null;
+    primary_web3_wallet_id:   null;
+    private_metadata:         Metadata;
+    profile_image_url:        string;
+    public_metadata:          Metadata;
+    totp_enabled:             boolean;
+    two_factor_enabled:       boolean;
+    unsafe_metadata:          Metadata;
+    updated_at:               number;
+    username:                 string;
+    web3_wallets:             any[];
+}
+
+export interface EmailAddress {
+    email_address: string;
+    id:            string;
+    linked_to:     null[];
+    object:        string;
+    reserved:      boolean;
+    verification:  null[];
+}
+
+export interface ExternalAccount {
+    approved_scopes:   string;
+    avatar_url:        string;
+    email_address:     string;
+    first_name:        string;
+    id:                string;
+    identification_id: string;
+    label:             null;
+    last_name:         string;
+    object:            string;
+    provider:          string;
+    provider_user_id:  string;
+    public_metadata:   Metadata;
+    username:          string;
+    verification:      null[];
+}
+
+export interface Metadata {
+}
