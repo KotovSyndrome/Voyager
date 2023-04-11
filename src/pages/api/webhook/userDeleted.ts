439090decaf298df7e1b006134414aa594fbac9c
Philip Ziolkowski <philipz4848@gmail.com>
2023-04-10 19:33:49 -0700

    Writing hook correctly

--- src/pages/api/webhook/userDeleted.ts
+++ src/pages/api/webhook/userDeleted.ts
@@ -16,7 +16,7 @@ type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
 };
   
 type Event = {
-    data: UserData;
+    data: UserDeletedData
     object: "event";
     type: EventType;
 };
@@ -46,17 +46,6 @@ export default async function handler(
 
   if (eventType === "user.deleted") {
     if (evt.data) {
-      console.log('evt.data: ', evt.data)
-
-      const emailObject = evt.data?.email_addresses?.find((email) => {
-        return email.id === evt?.data.primary_email_address_id;
-      });
-
-      if (!emailObject) {
-        console.log('Email not found')
-        return res.status(400).json({});
-      }
-
       await prisma.profile.delete({
         where: {
             clerkId: evt.data.id,
@@ -68,62 +57,8 @@ export default async function handler(
   }
 }
 
-export interface UserData {
-    backup_code_enabled:      boolean;
-    banned:                   boolean;
-    birthday:                 string;
-    created_at:               number;
-    email_addresses:          EmailAddress[];
-    external_accounts:        ExternalAccount[];
-    external_id:              null;
-    first_name:               string;
-    gender:                   string;
-    id:                       string;
-    image_url:                string;
-    last_name:                null;
-    last_sign_in_at:          null;
-    object:                   string;
-    password_enabled:         boolean;
-    phone_numbers:            any[];
-    primary_email_address_id: string;
-    primary_phone_number_id:  null;
-    primary_web3_wallet_id:   null;
-    private_metadata:         Metadata;
-    profile_image_url:        string;
-    public_metadata:          Metadata;
-    totp_enabled:             boolean;
-    two_factor_enabled:       boolean;
-    unsafe_metadata:          Metadata;
-    updated_at:               number;
-    username:                 string;
-    web3_wallets:             any[];
-}
-
-export interface EmailAddress {
-    email_address: string;
-    id:            string;
-    linked_to:     null[];
-    object:        string;
-    reserved:      boolean;
-    verification:  null[];
-}
-
-export interface ExternalAccount {
-    approved_scopes:   string;
-    avatar_url:        string;
-    email_address:     string;
-    first_name:        string;
-    id:                string;
-    identification_id: string;
-    label:             null;
-    last_name:         string;
-    object:            string;
-    provider:          string;
-    provider_user_id:  string;
-    public_metadata:   Metadata;
-    username:          string;
-    verification:      null[];
-}
-
-export interface Metadata {
+interface UserDeletedData {
+  deleted: boolean,
+  id: string,
+  object: string
 }
